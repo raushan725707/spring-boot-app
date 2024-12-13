0413f290ee9f84298091a78fdeb2db4fc1a3e379
Raushan Kumar <raushan.kumar@synlabs.io>
2024-12-13 11:46:02 +0530

    new commit

--- src/main/java/com/jenkins_docker/JenkinsDockerApplication.java
+++ src/main/java/com/jenkins_docker/JenkinsDockerApplication.java
@@ -2,12 +2,19 @@ package com.jenkins_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDockerApplication {
+public class JenkinsDockerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerApplication.class, args);
 	}
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsDockerApplication.class);
+	}
 }
